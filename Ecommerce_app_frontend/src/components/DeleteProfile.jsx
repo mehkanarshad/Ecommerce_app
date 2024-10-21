50c62b9b95b10ad34542592e657b7db2bd065d7e
mehkanarshad <mehkanarshadskp@gmail.com>
2024-10-21 21:26:57 +0500

    delete profile component added

--- Ecommerce_app_frontend/src/components/DeleteProfile.jsx
+++ Ecommerce_app_frontend/src/components/DeleteProfile.jsx
@@ -1,11 +1,38 @@
-import React from 'react'
-import axios from 'axios'
-import { useNavigate } from 'react-router-dom'
+import React from "react";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function DeleteProfile() {
+  const navigate = useNavigate();
+  const handleClick = async () => {
+    const accessToken = localStorage.getItem("access-token");
+    const uid = localStorage.getItem("uid");
+    const client = localStorage.getItem("client");
+
+    if (window.confirm("Are you sure you want to delete your profile")) {
+      try {
+        await axios.delete("http://localhost:3000/auth/", {
+          headers: {
+            "access-token": accessToken,
+            client: client,
+            uid: uid,
+          },
+        });
+
+        localStorage.removeItem("access-token");
+        localStorage.removeItem("client");
+        localStorage.removeItem("uid");
+
+        navigate("/login");
+      } catch (e) {
+        console.log("Delete Profile error", e);
+      }
+    }
+  };
+
   return (
     <div>
-      
+      <button onClick={handleClick}> Delete Profile </button>
     </div>
-  )
+  );
 }
--- Ecommerce_app_frontend/src/components/Home.jsx
+++ Ecommerce_app_frontend/src/components/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Logout from './Logout'
+import DeleteProfile from './DeleteProfile'
 
 export default function Home() {
   return (
     <div>
       <h1>Home</h1>
       <Logout/>
+      <DeleteProfile/>
     </div>
   )
 }
